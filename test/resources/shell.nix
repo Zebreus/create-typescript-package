c3b72349eb2d83f15efeb21331d8487401310891
Zebreus <lennarteichhorn@googlemail.com>
2022-07-20 20:54:51 +0200

    Adjust shell.nix

--- test/resources/shell.nix
+++ test/resources/shell.nix
@@ -4,11 +4,10 @@ mkShell {
   nativeBuildInputs = [
     nodejs-18_x
     yarn
-    terraform
     git
     jq
     moreutils
-  ] ++ (if stdenv.isDarwin then [ ] else [ docker docker-compose ]);
+  ];
   shellHook = with pkgs; ''
     export PATH="$(pwd)/node_modules/.bin:$PATH"
   '';
